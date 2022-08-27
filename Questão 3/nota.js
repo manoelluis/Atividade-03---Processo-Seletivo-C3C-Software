9518924db0025487cebc0d1f454bf3d6d4501acd
Manoel <manoelluisbusiness@gmail.com>
2022-08-27 14:16:48 -0300

    questão 3 quase pronta

--- Questão 3/nota.js
+++ Questão 3/nota.js
@@ -3,23 +3,28 @@ const resultado = document.querySelector("#resultado");
 const calcular = document.querySelector("#calcular");
 
 calcular.onclick = () => {
-  const corrigida = arredondar();
+  const valor = Math.floor(Number(nota.value));
 
-  if (nota.value < 0 || nota.value > 100) {
+  const corrigida = arredondar(valor);
+  if (valor < 0 || valor > 100) {
     alert("Digite um numero entre 0 e 100");
-  } else if (corrigida < 40) {
-    resultado.value = "Voce foi reprovado :(";
+    return;
+  }
+  if (corrigida < 40) {
+    resultado.value = `Voce foi reprovado com a nota ${corrigida}`;
   } else {
-    resultado.value = "Voce foi aprovado! :)";
+    resultado.value = `Voce foi aprovado com a nota ${corrigida}`;
   }
 };
 
-const arredondar = () => {
-  if (nota.value < 38) {
-    return nota.value;
-  } else if (nota.value % 5 >= 3) {
-    return (nota.value = nota.value + (5 - (nota.value % 5)));
-  } else {
-    return (nota.value = nota.value - (nota.value % 5));
+const arredondar = (valor) => {
+  if (valor < 38) {
+    return valor;
   }
+
+  if (valor % 5 >= 3) {
+    return valor + (5 - (valor % 5));
+  }
+
+  return valor;
 };
