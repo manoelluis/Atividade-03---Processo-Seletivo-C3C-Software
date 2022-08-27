cd99f85a5c92e28772c8216d4918799522ddeecd
Manoel <manoelluisbusiness@gmail.com>
2022-08-27 09:18:04 -0300

    iniciando questão 2

---
+++ Questão 2/bhaskara.js
@@ -0,0 +1,5 @@
+const n1 = document.querySelector("#n1");
+const n2 = document.querySelector("#n2");
+const n3 = document.querySelector("#n3");
+const calcular = document.querySelector("#calcular");
+const resultado = document.querySelector("#resultado");
